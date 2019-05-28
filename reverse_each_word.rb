a4f13e46fc6d3103888f517abfd5458d08d331e1
Kelly Dobbins <kellydobbins26@outlook.com>
2019-05-28 08:22:17 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,25 +1,9 @@
-def reverse_each_word(array)
-array = []
-array.insert(0, "Hello and how are you?")
-end 
-
-def using_each(word)
-array.each do |word|
-word.reverse
-"'#{[].join(",")}'"
-end 
-
-def new_array(array2)
-array2 = []
-array2.insert(0, "Hi again, just making sure it's reversed!")
-end 
-
-def using_each_again(word)
-array.each do |word|
-  word.reverse
-  "'#{[].join(",")}"
-end 
-end 
-
-def using_collect_method
+    
+def reverse_each_word(sentence)
+  sentence.split.collect {|word| word.reverse}.join(" ")
 end
+
+def reverse_each_word(sentence)
+  sentence.reverse.split.reverse.join(" ")
+end 
+end 
